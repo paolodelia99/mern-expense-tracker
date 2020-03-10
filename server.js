5c26d1ee0931728f21863cc16a4da946e5a037df
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-10 13:58:08 +0100

    getting ready for the deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const path = require('path');
 const morgan = require('morgan');
 const connectDb = require('./config/db');
 
