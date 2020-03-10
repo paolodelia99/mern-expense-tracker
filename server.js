6adcc9d31091207532ff93c3faacf8ea3a8fdbd1
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-10 12:23:38 +0100

    Getting ready for heroku

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ connectDb();
 const app = express();
 
 //Body parsers middleware
-app.use(express.json());
+app.use(express.json({extended: false}));
 
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
+} else if (process.env.NODE_ENV === 'production') {
+        // Set static folder
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
 }
 
 //Routes
